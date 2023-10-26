4b51d8edbacee4249b42cda09a3e8c90d0d6b04e
zc <1972862096@qq.com>
2023-10-26 16:55:31 +0800

    zc,init,feat: init project

---
+++ chatbot-api-interfaces/src/test/java/org/example/chatbot/api/ApiTest.java
@@ -0,0 +1,13 @@
+package org.example.chatbot.api;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ApiTest {
+
+    @Test
+    public void test(){
+        System.out.println("1");
+    }
+}
